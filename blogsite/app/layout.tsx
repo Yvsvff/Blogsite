6edef93bb1f1093cef697c90b93c4f9f8d7e53df
Yusuff Atanda <Yvsvfff@gmail.com>
2023-09-30 05:35:10 -0400

    add image

---
+++ blogsite/app/layout.tsx
@@ -0,0 +1,26 @@
+
+import './globals.css'
+import type { Metadata } from 'next'
+
+
+
+
+export const metadata: Metadata = {
+  
+  title: 'JB Blog',
+  description: 'Created by Yusuff Atanda',
+ 
+
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body >{children}</body>
+    </html>
+  )
+}
